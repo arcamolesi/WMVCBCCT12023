666f9990c9dad62ae793e157dba18c2d690ecb3d
Almir Rogério Camolesi <camolesi@fema.edu.br>
2023-09-04 17:58:20 -0300

    Atualiza Labs Index

---
+++ Controllers/LabsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WMVCBCCT12023.Models;
+
+namespace WMVCBCCT12023.Controllers
+{
+    public class LabsController : Controller
+    {
+        private readonly Contexto _context;
+       
+        public LabsController(Contexto contexto)
+        {
+            _context = contexto;
+        }
+
+
+        // GET: Salas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Salas.ToListAsync());
+        }
+
+    }
+}
